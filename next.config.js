fff3b657c9a6ec815ae65eacf73e28f4914fc022
Lucas (Lightz) <sky.lucas003@gmail.com>
2024-12-05 10:21:12 -0300

    build: update next.config.js and package.json to adjust experimental settings and add node engine specification

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
+  compress: false,
 };
 
 module.exports = nextConfig;
