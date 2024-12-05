c30558e00b107635b84f89b9ca25049c81a1b72d
Lucas (Lightz) <sky.lucas003@gmail.com>
2024-12-05 10:48:30 -0300

    build: remove experimental settings from next.config.js and update node engine to 22.x in package.json

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
-    workerThreads: false,
-    cpus: 1,
-  },
-  compress: false,
 };
 
 module.exports = nextConfig;
