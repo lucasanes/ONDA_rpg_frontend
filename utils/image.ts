8c6482fb899316d552270682082ae518da9a89e7
Lucas (Lightz) <sky.lucas003@gmail.com>
2024-12-23 02:23:09 -0300

    refactor: remove unused moldure position constants and adjust portrait padding

--- utils/image.ts
+++ utils/image.ts
@@ -2,6 +2,3 @@ export const PORTRAIT_SIZE = 600;
 
 export const MOLDURE_1_IMAGE_SIZE = 600;
 export const MOLDURE_2_IMAGE_SIZE = 550;
-
-export const MOLDURE_1_IMAGE_POSITION = 'left-6 top-6';
-export const MOLDURE_2_IMAGE_POSITION = 'left-14 top-14';
